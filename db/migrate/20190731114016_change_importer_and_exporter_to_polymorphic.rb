4ad295999ff6c2f095ad3dc2fa67cd9a05f6b77f
Thomas Scherz <scherztc@ucmail.uc.edu>
2020-09-25 11:27:21 -0400

    Removes rename and uses create for importerexporter.

--- db/migrate/20190731114016_change_importer_and_exporter_to_polymorphic.rb
+++ db/migrate/20190731114016_change_importer_and_exporter_to_polymorphic.rb
@@ -6,7 +6,7 @@ end
 
 class ChangeImporterAndExporterToPolymorphic < ActiveRecord::Migration[5.1]
   def change
-    ActiveRecord::Base.connection.execute "ALTER TABLE `bulkrax_entries` CHANGE `importer_id` `importerexporter_id` bigint(20)"
+    # ActiveRecord::Base.connection.execute "ALTER TABLE `bulkrax_entries` CHANGE `importer_id` `importerexporter_id` bigint(20)"
     add_column :bulkrax_entries, :importerexporter_type, :string, after: :id, default: 'Bulkrax::Importer' unless column_exists?(:bulkrax_entries, :importerexporter_type)
   end
 end
